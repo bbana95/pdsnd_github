24f7f0fed66468921a34fa6297f0b539dc0accd5
Ji Soo Ha <bbana95@gmail.com>
2021-04-09 17:33:13 +0200

    Add a description about the datatype of CITY_DATA

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-
+#the type of CITY_DATA is dict
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
