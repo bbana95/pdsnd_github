06996169cfc8790d578a45a419d1c942dd6c6351
Ji Soo Ha <bbana95@gmail.com>
2021-04-09 17:41:06 +0200

    Add a description about a function restart

--- bikeshare.py
+++ bikeshare.py
@@ -211,7 +211,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
+#As long as you click 'yes', the process is continously repeated
 
 if __name__ == "__main__":
 	main()
